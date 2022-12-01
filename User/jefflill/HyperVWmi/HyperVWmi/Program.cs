9a34863e8baac112f7f723a03242dd34f818c173
Jeff Lill <jeff@lilltek.com>
2022-12-01 13:09:08 -0800

    [jeff]: Hyper-V WMI experimentation

---
+++ User/jefflill/HyperVWmi/HyperVWmi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Management;
+
+namespace Neon.HyperV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var hypervWmi = new HyperVWmi())
+                {
+                    hypervWmi.ValidateDisk(@"C:\Temp\test.vhdx");
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
