d7bb5f5da53c3a9356134909506285f3bae240ec
Jeff Lill <jeff@lilltek.com>
2022-12-26 15:21:38 -0800

    [Neon.Git] in progress

--- Lib/Neon.Git/GitHub/GitHubExtensions.cs
+++ Lib/Neon.Git/GitHub/GitHubExtensions.cs
@@ -130,11 +130,98 @@ namespace Neon.Git
         }
 
         /// <summary>
-        /// Returns all branches for a GitHub repo.
+        /// Clones a remote GitHub repo to a local directory.
         /// </summary>
         /// <param name="githubClient">Specifies the GitHub client.</param>
         /// <param name="remoteRepo">Specifies the remote (GitHub) repository path, like: <b>[SERVER/]OWNER/REPO</b></param>
-        public static async Task<IReadOnlyList<GitHubBranch>> ListBanchesAsync(this GitHubClient githubClient, string remoteRepo)
+        /// <param name="localRepoPath">Specifies the local repo directory.</param>
+        /// <param name="branchName">
+        /// Optionally specifies the branch to be checked out after the clone operation completes.
+        /// This defaults to the remote repos default branch (typically <b>main</b> or <b>master</b>).
+        /// </param>
+        /// <returns>The tracking <see cref="Task"/>.</returns>
+        public static async Task CloneAsync(this GitHubClient githubClient, string remoteRepo, string localRepoPath, string branchName = null)
+        {
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(remoteRepo), nameof(remoteRepo));
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoPath), nameof(localRepoPath));
+            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoPath), $"Local git repo does not exist at: {localRepoPath}");
+
+            GitHubRepoPath.Parse(remoteRepo);     // Validate the repo path
+
+            var remoteUri = $"github.com/{remoteRepo}";
+            var options   = new CloneOptions() { BranchName = branchName };
+
+            GitRepository.Clone(remoteUri, localRepoPath, options);
+
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Checks out a local repo branch.
+        /// </summary>
+        /// <param name="githubClient">Specifies the GitHub client.</param>
+        /// <param name="localRepoPath">Specifies the local repo directory.</param>
+        /// <param name="branchName">
+        /// Optionally specifies the branch to be checked out after the clone operation completes.
+        /// This defaults to the remote repos default branch (typically <b>main</b> or <b>master</b>).
+        /// <param name="branchName"></param>
+        /// <returns>The tracking <see cref="Task"/>.</returns>
+        public static async Task CheckoutAsync (this GitHubClient githubClient, string localRepoPath, string branchName)
+        {
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoPath), nameof(localRepoPath));
+            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoPath), $"Local git repo does not exist at: {localRepoPath}");
+
+            using (var repo = new GitRepository(localRepoPath))
+            {
+                var branch = repo.Branches.SingleOrDefault(branch => branch.FriendlyName == branchName);
+
+                if (branch == null)
+                {
+                    throw new InvalidOperationException($"Branch [{branchName}] does not exist.");
+                }
+
+                Commands.Checkout(repo, branch);
+            }
+
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Fetches a local repo from the remote.
+        /// </summary>
+        /// <param name="githubClient">Specifies the GitHub client.</param>
+        /// <param name="localRepoPath">Specifies the local repo directory.</param>
+        /// <returns>The tracking <see cref="Task"/>.</returns>
+        public static async Task FetchAsync(this GitHubClient githubClient, string localRepoPath)
+        {
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoPath), nameof(localRepoPath));
+            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoPath), $"Local git repo does not exist at: {localRepoPath}");
+
+            using (var repo = new GitRepository(localRepoPath))
+            {
+                var options = new FetchOptions()
+                {
+                    TagFetchMode        = TagFetchMode.Auto,
+                    Prune               = true,
+                    CredentialsProvider = CredentialsProvider
+                };
+
+                var remote   = repo.Network.Remotes["origin"];
+                var refSpecs = remote.FetchRefSpecs.Select(spec => spec.Specification);
+
+                Commands.Fetch(repo, remote.Name, refSpecs, options, "fetching");
+
+                await Task.CompletedTask;
+            }
+        }
+
+        /// <summary>
+        /// Returns the remote branches for a GitHub repo.
+        /// </summary>
+        /// <param name="githubClient">Specifies the GitHub client.</param>
+        /// <param name="remoteRepo">Specifies the remote (GitHub) repository path, like: <b>[SERVER/]OWNER/REPO</b></param>
+        /// <returns>The list of branches.</returns>
+        public static async Task<IReadOnlyList<GitHubBranch>> ListRemoteBanchesAsync(this GitHubClient githubClient, string remoteRepo)
         {
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(remoteRepo), nameof(remoteRepo));
 
@@ -147,17 +234,17 @@ namespace Neon.Git
         /// Commits any staged and pending changes to the local git repo.
         /// </summary>
         /// <param name="githubClient">Specifies the GitHub client.</param>
-        /// <param name="localRepoDirectory">Path to the root of the local Git repo.</param>
+        /// <param name="localRepoPath">Path to the root of the local Git repo.</param>
         /// <param name="message">Optionally specifies the commit message.  This defaults to <b>unspecified changes"</b>.</param>
         /// <returns><c>true</c> when changes were comitted, <c>false</c> when there were no pending changes.</returns>
         /// <exception cref="InvalidOperationException">Thrown when the operation fails.</exception>
-        public static async Task<bool> CommitAsync(this GitHubClient githubClient, string localRepoDirectory, string message = "unspecified changes")
+        public static async Task<bool> CommitAsync(this GitHubClient githubClient, string localRepoPath, string message = "unspecified changes")
         {
-            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoDirectory));
-            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoDirectory), $"Local git repo does not exist at: {localRepoDirectory}");
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoPath));
+            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoPath), $"Local git repo does not exist at: {localRepoPath}");
             Covenant.Requires<ArgumentNullException>(string.IsNullOrEmpty(message), nameof(message));
 
-            using (var gitRepo = new GitRepository(localRepoDirectory))
+            using (var gitRepo = new GitRepository(localRepoPath))
             {
                 if (!gitRepo.RetrieveStatus().IsDirty)
                 {
@@ -174,7 +261,7 @@ namespace Neon.Git
 
                 if (remote == null)
                 {
-                    throw new InvalidOperationException($"Local git repo [{localRepoDirectory}] has no remote origin.");
+                    throw new InvalidOperationException($"Local git repo [{localRepoPath}] has no remote origin.");
                 }
             }
 
@@ -182,17 +269,23 @@ namespace Neon.Git
         }
 
         /// <summary>
+        /// <para>
         /// Pulls the changes from GitHub into the checked-out branch.
+        /// </para>
+        /// <note>
+        /// The pull operation will be aborted and rolled back for merge conflicts.
+        /// Check the result status to understand what happened.
+        /// </note>
         /// </summary>
         /// <param name="githubClient">Specifies the GitHub client.</param>
-        /// <param name="localRepoDirectory">Specifies the path to the root of the local Git repo.</param>
-        /// <returns></returns>
-        public static async Task PullAsync(this GitHubClient githubClient, string localRepoDirectory)
+        /// <param name="localRepoPath">Specifies the path to the root of the local Git repo.</param>
+        /// <returns>The <see cref="MergeStatus"/> for the operation.</returns>
+        public static async Task<MergeStatus> PullAsync(this GitHubClient githubClient, string localRepoPath)
         {
-            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoDirectory));
-            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoDirectory), $"Local git repo does not exist at: {localRepoDirectory}");
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoPath));
+            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoPath), $"Local git repo does not exist at: {localRepoPath}");
 
-            using (var gitRepo = new GitRepository(localRepoDirectory))
+            using (var gitRepo = new GitRepository(localRepoPath))
             {
                 var options = new PullOptions()
                 {
@@ -202,7 +295,7 @@ namespace Neon.Git
                      }
                 };
 
-                Commands.Pull(gitRepo, new GitSignature(Username, Email, DateTimeOffset.Now), options);
+                return Commands.Pull(gitRepo, new GitSignature(Username, Email, DateTimeOffset.Now), options).Status;
             }
 
             await Task.CompletedTask;
@@ -212,20 +305,20 @@ namespace Neon.Git
         /// Pushes any pending local commits from the checked out branch to GitHub.
         /// </summary>
         /// <param name="githubClient">The GitHub client.</param>
-        /// <param name="localRepoDirectory">Path to the root of the local Git repo.</param>
+        /// <param name="localRepoPath">Path to the root of the local Git repo.</param>
         /// <returns><c>true</c> when commits were pushed, <c>false</c> when there were no pending commits.</returns>
-        public static async Task<bool> PushAsync(this GitHubClient githubClient, string localRepoDirectory)
+        public static async Task<bool> PushAsync(this GitHubClient githubClient, string localRepoPath)
         {
-            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoDirectory));
-            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoDirectory), $"Local git repo does not exist at: {localRepoDirectory}");
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(localRepoPath));
+            Covenant.Requires<ArgumentException>(Directory.Exists(localRepoPath), $"Local git repo does not exist at: {localRepoPath}");
 
-            using (var gitRepo = new GitRepository(localRepoDirectory))
+            using (var gitRepo = new GitRepository(localRepoPath))
             {
                 var remote = gitRepo.Network.Remotes["origin"];
 
                 if (remote == null)
                 {
-                    throw new InvalidOperationException($"Local git repo [{localRepoDirectory}] has no remote origin.");
+                    throw new InvalidOperationException($"Local git repo [{localRepoPath}] has no remote origin.");
                 }
 
                 var options = new PushOptions()
@@ -237,7 +330,7 @@ namespace Neon.Git
 
                 if (currentBranch == null)
                 {
-                    throw new InvalidOperationException($"Local git repo [{localRepoDirectory}] has no checked-out branch.");
+                    throw new InvalidOperationException($"Local git repo [{localRepoPath}] has no checked-out branch.");
                 }
 
                 if (gitRepo.Commits.Count() == 0)
