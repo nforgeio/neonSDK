a48a1fc1c4fa5c1d4311ddc831316f84d61ce39d
Jeff Lill <jeff@lilltek.com>
2023-02-17 20:53:30 -0800

    [build]: fixed warning

--- Lib/Neon.Service/LogMetricsProcessor.cs
+++ Lib/Neon.Service/LogMetricsProcessor.cs
@@ -52,7 +52,7 @@ namespace Neon.Service
     /// errors, requiring further investigation.
     /// </para>
     /// <para>
-    /// This is very easy to use.  Simply call <see cref="OtelConfigExtensions.AddLogMetricsProcessor(OpenTelemetryLoggerOptions)"/>.
+    /// This is very easy to use.  Simply call <see cref="OtelConfigExtensions.AddLogMetricsProcessor(OpenTelemetryLoggerOptions, string)"/>.
     /// </para>
     /// </remarks>
     public class LogMetricsProcessor : BaseProcessor<LogRecord>
