680cdc862fbcd8d8cb392415bac44f16e060fbff
Jeff Lill <jeff@lilltek.com>
2023-03-07 12:18:35 -0800

    AwsCli.S3UploadMultiPart() ony updates progress for even part numbers to avoid confusion

--- Lib/Neon.Deployment/Aws/AwsCli.cs
+++ Lib/Neon.Deployment/Aws/AwsCli.cs
@@ -756,10 +756,11 @@ namespace Neon.Deployment
                             partStart   += partSize;
                             cbRemaining -= partSize;
 
-                            if (progressAction != null)
-                            {
-                                progressAction(Math.Min(99L, (long)(100.0 * (double)partNumber / (double)partCount)));
-                            }
+                            // $hack(jefflill):
+                            //
+                            // We're only invoking the progress action for even parts above
+                            // to avoid confustion when it appears that progress is bouncing
+                            // around.
                         }
                     }
                 });
