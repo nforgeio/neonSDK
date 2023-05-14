7c263949885941ad0c07e22d5dbfc9879257c21f
Jeff Lill <jeff@lilltek.com>
2023-05-14 09:40:56 -0700

    [refactor/term]: don't use "built-in" when referring tom NEONDESKTOP clusters

--- Test/Test.Neon.Common/Net/Test_NetHelper.cs
+++ Test/Test.Neon.Common/Net/Test_NetHelper.cs
@@ -546,8 +546,8 @@ namespace TestCommon
             Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }, ReachableHostMode.ReturnFirst).Host);
 
             // The [100.64.0.0/20] subnet is never supposed to be routable although NEONKUBE
-            // does use 100.64.0.0/24 for NEONDESKTOP built-in (an other internal clusters)
-            // so we'll use some addresses at the upper end of 100.64.0.0/20.
+            // does use 100.64.0.0/24 for NEONDESKTOP (an other internal clusters) so we'll
+            // use some addresses at the upper end of 100.64.0.0/20.
 
             const string badIP0 = "100.64.15.252";
             const string badIP1 = "100.64.15.253";
@@ -608,8 +608,8 @@ namespace TestCommon
             }
 
             // The [100.64.0.0/20] subnet is never supposed to be routable although NEONKUBE
-            // does use 100.64.0.0/24 for NEONDESKTOP built-in (an other internal clusters)
-            // so we'll use some addresses at the upper end of 100.64.0.0/20 instead.
+            // does use 100.64.0.0/24 for NEONDESKTOP (an other internal clusters) so we'll
+            // use some addresses at the upper end of 100.64.0.0/20 instead.
 
             const string badIP0 = "100.64.15.252";
             const string badIP1 = "100.64.15.253";
