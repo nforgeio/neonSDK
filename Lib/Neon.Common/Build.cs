5a900be0068f6ce5c8845107a29d30a468242e89
Jeff Lill <jeff@lilltek.com>
2023-12-09 12:36:53 -0800

    Directory.Build.props: bump SDK version to: 4.0.0-rc.1

--- Lib/Neon.Common/Build.cs
+++ Lib/Neon.Common/Build.cs
@@ -57,7 +57,7 @@ namespace Neon
         /// release scripts to match.
         /// </note>
         /// </summary>
-        public const string NeonSdkVersion = "4.0.0-rc.0";
+        public const string NeonSdkVersion = "4.0.0-rc.1";
 
         /// <summary>
         /// The product license.
