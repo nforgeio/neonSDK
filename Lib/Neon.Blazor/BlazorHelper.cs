ae9c55cbe746d4afe72fad9ef4c99f7b17e7f748
Marcus Bowyer <marcus@euarctos.com>
2024-01-24 12:54:43 -0800

    The most significant change is the addition of a new file, `BlazorHelper.cs`, to the project. This file contains a static class `BlazorHelper` that includes a method `GeneratePath<T>`. This method is designed to generate a Blazor component path from the component type and parameters, using reflection to get the `RouteAttribute` from the component type and constructing the path using the route template and provided parameters.
    Changes:
    - A new file `BlazorHelper.cs` was added to the project. This file is part of the `Neon.Blazor` namespace.
    - The `BlazorHelper.cs` file contains a static class `BlazorHelper`.
    - The `BlazorHelper` class includes a method `GeneratePath<T>`. This method generates a Blazor component path from the component type and parameters.
    - The `GeneratePath<T>` method uses reflection to get the `RouteAttribute` from the component type.
    - The `GeneratePath<T>` method constructs the path using the route template and provided parameters.
    - The `BlazorHelper.cs` file includes a copyright notice and a license notice.

---
+++ Lib/Neon.Blazor/BlazorHelper.cs
@@ -0,0 +1,61 @@
+// -----------------------------------------------------------------------------
+// FILE:	    BlazorHelper.cs
+// CONTRIBUTOR: NEONFORGE Team
+// COPYRIGHT:   Copyright © 2005-2023 by NEONFORGE LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Reflection;
+using System.Text;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Neon.Blazor
+{
+    public static class BlazorHelper
+    {
+        /// <summary>
+        /// Generates a Blazor component path from the component type and the parameters.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static string GeneratePath<T>(params object[] values)
+            where T : IComponent
+        {
+            var route = typeof(T).GetCustomAttribute<RouteAttribute>();
+
+            var sb = new StringBuilder();
+            sb.Append("/");
+
+            int index = 0;
+            foreach (var part in route.Template.Split('/', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            {
+                if (part.StartsWith('{') && part.EndsWith('}'))
+                {
+                    sb.Append(values[index]);
+                    index++;
+                }
+                else
+                {
+                    sb.Append(part);
+                }
+
+                sb.Append("/");
+            }
+
+            return sb.ToString().TrimEnd('/');
+        }
+    }
+}
