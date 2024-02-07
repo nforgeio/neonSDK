8f56b80f58d37dd68b9d374905c8ae14dd05b9b5
Marcus Bowyer <marcus@euarctos.com>
2024-02-07 15:10:06 -0800

    upgrade neonsdk

--- Lib/Neon.Common/Build.cs
+++ Lib/Neon.Common/Build.cs
@@ -51,7 +51,7 @@ namespace Neon
         /// release scripts to match.
         /// </note>
         /// </summary>
-        public const string NeonSdkVersion = "4.0.0-rc.16";
+        public const string NeonSdkVersion = "4.0.0-rc.17";
 
         /// <summary>
         /// The product license.
