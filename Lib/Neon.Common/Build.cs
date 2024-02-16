7e5929c87597fc06f9eaf13ceb9e2256f39999f8
Marcus Bowyer <marcus@euarctos.com>
2024-02-16 13:03:42 -0800

    Directory.Build.props: bump SDK version to: 4.0.4-rc.1

--- Lib/Neon.Common/Build.cs
+++ Lib/Neon.Common/Build.cs
@@ -51,7 +51,7 @@ namespace Neon
         /// release scripts to match.
         /// </note>
         /// </summary>
-        public const string NeonSdkVersion = "4.0.3";
+        public const string NeonSdkVersion = "4.0.4-rc.1";
 
         /// <summary>
         /// The product license.
