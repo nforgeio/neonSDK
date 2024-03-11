594b866ceacc97b64407d6ca66a4ce7d6d2b8a4d
Jeff Lill <jeff@lilltek.com>
2024-03-10 17:06:29 -0700

    [neon-docs]: related configuration and documentation

--- buildenv.cmd
+++ buildenv.cmd
@@ -22,7 +22,7 @@ if not [%1]==[] set NF_ROOT=%1
 
 if exist %NF_ROOT%\neonSDK.sln goto goodPath
 echo The [%NF_ROOT%\neonSDK.sln] file does not exist.  Please pass the path
-echo to the neonSDK solution folder.
+echo to the NEONSDK solution folder.
 goto done
 
 :goodPath 
