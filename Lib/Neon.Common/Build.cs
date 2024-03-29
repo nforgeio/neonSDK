1de7e0eaa3d255183444cad39356393941c712fc
Marcus Bowyer <marcus@euarctos.com>
2024-03-29 12:48:54 -0700

    Updated Neon SDK version in Build.cs and Directory.Build.props
    Updated the Neon SDK version from 4.0.7 to 4.0.8 in the `Build.cs` file under the `Neon` namespace. Also, updated the Neon SDK package version from 4.0.6 to 4.0.8 in the `Directory.Build.props` file.

--- Lib/Neon.Common/Build.cs
+++ Lib/Neon.Common/Build.cs
@@ -51,7 +51,7 @@ namespace Neon
         /// release scripts to match.
         /// </note>
         /// </summary>
-        public const string NeonSdkVersion = "4.0.7";
+        public const string NeonSdkVersion = "4.0.8";
 
         /// <summary>
         /// The product license.
