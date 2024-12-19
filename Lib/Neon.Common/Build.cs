739262d322c896545b523da2af6efba6b03083e5
Marcus Bowyer <marcus@euarctos.com>
2024-12-19 13:15:43 -0800

    Update NeonSdkVersion to stable release 4.0.14
    The constant `NeonSdkVersion` in the `Neon` namespace within the `Build.cs` file has been updated from `"4.0.13-preview.28"` to `"4.0.14"`. This change indicates a transition from a preview version to a stable release of the SDK.

--- Lib/Neon.Common/Build.cs
+++ Lib/Neon.Common/Build.cs
@@ -51,7 +51,7 @@ namespace Neon
         /// release scripts to match.
         /// </note>
         /// </summary>
-        public const string NeonSdkVersion = "4.0.13-preview.28";
+        public const string NeonSdkVersion = "4.0.14";
 
         /// <summary>
         /// The product license.
