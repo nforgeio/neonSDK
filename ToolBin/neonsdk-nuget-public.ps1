00feeba3cd6c183471f7492b95091d4ae686d0f0
Marcus Bowyer <marcus@euarctos.com>
2025-11-26 13:51:56 -0800

    Add publishing for Neon.ECharts and Neon.Mapbox
    Added support for publishing the `Neon.ECharts` and `Neon.Mapbox`
    packages in the `neonsdk-nuget-public.ps1` script. These packages
    will now be published using the `$neonSdkVersion` variable,
    ensuring consistency with other published packages.

--- ToolBin/neonsdk-nuget-public.ps1
+++ ToolBin/neonsdk-nuget-public.ps1
@@ -173,10 +173,12 @@ try
     Publish Neon.CSharp                 $neonSdkVersion
     Publish Neon.Deployment             $neonSdkVersion
     Publish Neon.Docker                 $neonSdkVersion
+    Publish Neon.ECharts                $neonSdkVersion
     Publish Neon.EntityFrameworkCore    $neonSdkVersion
     Publish Neon.GitHub                 $neonSdkVersion
     Publish Neon.HyperV                 $neonSdkVersion
     Publish Neon.JsonConverters         $neonSdkVersion
+    Publish Neon.Mapbox                 $neonSdkVersion
     Publish Neon.ModelGen               $neonSdkVersion
     Publish Neon.ModelGenerator         $neonSdkVersion
     Publish Neon.Nats                   $neonSdkVersion
